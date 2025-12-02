c100c0bf398dbe5c206abe4c71801207222978d2
Marin Atanasov Nikolov <dnaeon@gmail.com>
2025-12-02 10:14:38 +0200

    apis/config/validation: validate postgres config spec

---
+++ pkg/apis/config/validation/validation.go
@@ -0,0 +1,33 @@
+package validation
+
+import (
+	"github.com/gardener/gardener-extension-postgres-operator/pkg/apis/config"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+// Validate validates the given [config.PostgresConfig]
+func Validate(cfg config.PostgresConfig) error {
+	allErrs := make(field.ErrorList, 0)
+
+	if cfg.Spec.VolumeSize.IsZero() {
+		allErrs = append(allErrs, field.Required(field.NewPath("spec.volumeSize"), "no volume size specified"))
+	}
+
+	if cfg.Spec.Replicas <= 0 {
+		allErrs = append(allErrs, field.Invalid(field.NewPath("spec.replicas"), cfg.Spec.Replicas, "invalid number of replicas"))
+	}
+
+	if len(cfg.Spec.Users) == 0 {
+		allErrs = append(allErrs, field.Required(field.NewPath("spec.users"), "no users specified"))
+	}
+
+	if len(cfg.Spec.Databases) == 0 {
+		allErrs = append(allErrs, field.Required(field.NewPath("spec.databases"), "no databases specified"))
+	}
+
+	if cfg.Spec.PostgresVersion == "" {
+		allErrs = append(allErrs, field.Required(field.NewPath("spec.postgresVersion"), "no postgres version specified"))
+	}
+
+	return allErrs.ToAggregate()
+}
