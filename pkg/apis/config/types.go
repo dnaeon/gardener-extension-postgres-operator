7164aaf1c262b1c8a9b589310be87ce7f8a42831
Marin Atanasov Nikolov <dnaeon@gmail.com>
2025-12-02 10:14:11 +0200

    apis/config: add postgres config spec types

--- pkg/apis/config/types.go
+++ pkg/apis/config/types.go
@@ -5,23 +5,35 @@
 package config
 
 import (
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// ExampleConfigSpec defines the desired state of [ExampleConfig]
-type ExampleConfigSpec struct {
-	// Foo is foo
-	Foo string
+// PostgresConfigSpec defines the desired state of [PostgresConfig]
+type PostgresConfigSpec struct {
+	// VolumeSize specifies the size of the persistent volume for the
+	// cluster nodes.
+	VolumeSize resource.Quantity
 
-	// TODO(user): insert additional spec fields
+	// Replicas specifies the number of cluster instances.
+	Replicas int
+
+	// Users specifies the database users and their roles.
+	Users map[string][]string
+
+	// Databases specifies the database names and their owners.
+	Databases map[string]string
+
+	// PostgresVersion specifies the PostgreSQL version of the cluster.
+	PostgresVersion string
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// ExampleConfig is the schema for the exampleconfigs API
-type ExampleConfig struct {
+// PostgresConfig is the schema for the PostgresConfig API
+type PostgresConfig struct {
 	metav1.TypeMeta
 
 	// Spec provides the extension configuration spec.
-	Spec ExampleConfigSpec
+	Spec PostgresConfigSpec
 }
--- pkg/apis/config/v1alpha1/types.go
+++ pkg/apis/config/v1alpha1/types.go
@@ -5,23 +5,35 @@
 package v1alpha1
 
 import (
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// ExampleConfigSpec defines the desired state of [ExampleConfig]
-type ExampleConfigSpec struct {
-	// Foo is foo
-	Foo string `json:"foo,omitzero"`
+// PostgresConfigSpec defines the desired state of [PostgresConfig]
+type PostgresConfigSpec struct {
+	// VolumeSize specifies the size of the persistent volume for the
+	// cluster nodes.
+	VolumeSize resource.Quantity `json:"volumeSize,omitZero"`
 
-	// TODO(user): insert additional spec fields
+	// Replicas specifies the number of cluster instances.
+	Replicas int `json:"replicas,omitzero"`
+
+	// Users specifies the database users and their roles.
+	Users map[string][]string `json:"users,omitzero"`
+
+	// Databases specifies the database names and their owners.
+	Databases map[string]string `json:"databases,omitzero"`
+
+	// PostgresVersion specifies the PostgreSQL version of the cluster.
+	PostgresVersion string `json:"postgresVersion,omitzero"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// ExampleConfig is the schema for the exampleconfigs API
-type ExampleConfig struct {
+// PostgresConfig is the schema for the PostgresConfig API
+type PostgresConfig struct {
 	metav1.TypeMeta `json:",inline"`
 
 	// Spec provides the extension configuration spec.
-	Spec ExampleConfigSpec `json:"spec,omitzero"`
+	Spec PostgresConfigSpec `json:"spec,omitzero"`
 }
